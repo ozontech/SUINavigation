635341c7c9a8a5bd2bfbd5bccc012a85d6006311
Sergey Balalaev <sof.bix@mail.ru>
2024-02-26 15:50:25 +0400

    mark error step

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -2,7 +2,7 @@
 
 checkExit(){
     if [ $? != 0 ]; then
-        echo "Building failed\n"
+        echo "Building failed: $1\n"
         exit 1
     fi
 }
@@ -20,24 +20,24 @@ APP_CONFIG_PATH="./build.config"
 echo "CURRENT_VERSION=$CURRENT_VERSION" > "$APP_CONFIG_PATH"
 
 cd Example/NavigationExample
-checkExit
+checkExit "Start Tests"
 
 xcrun simctl shutdown all
 sh ../../Scripts/build.sh -c Debug -p NavigationExample -test 'platform=iOS Simulator,name=iPhone 15,OS=17.2'
-checkExit
+checkExit "Tests on iOS 17.2"
 
 xcrun simctl shutdown all
 sh ../../Scripts/build.sh -c Debug -p NavigationExample -test 'platform=iOS Simulator,name=iPhone 15,OS=17.0.1'
-checkExit
+checkExit "Tests on iOS 17.0.1"
 
 xcrun simctl shutdown all
 sh ../../Scripts/build.sh -c Debug -p NavigationExample -test 'platform=iOS Simulator,name=iPhone 14,OS=16.4'
-checkExit
+checkExit "Tests on iOS 16.4"
 
 xcrun simctl shutdown all
 sh ../../Scripts/build.sh -c Debug -p NavigationExample -test 'platform=iOS Simulator,name=iPhone 14,OS=16.0'
-checkExit
+checkExit "Tests on iOS 16.0"
 
 xcrun simctl shutdown all
 sh ../../Scripts/build.sh -c Debug -p NavigationExample -test 'platform=iOS Simulator,name=iPhone 13,OS=15.2'
-checkExit
+checkExit "Tests on iOS 15.2"
