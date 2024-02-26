5d8d93d4bfb137d7e216b8301d1c3b6fa4c3c8b1
Sergey Balalaev <sof.bix@mail.ru>
2024-02-26 12:22:17 +0400

    try to fix build on build machine

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+checkExit(){
+    if [ $? != 0 ]; then
+        echo "Building failed\n"
+        exit 1
+    fi
+}
+
 CHANGELOG=$(cat CHANGELOG.md)
 VERSION_REGEX='## \[([.0-9]*)\]'
 CURRENT_VERSION=""
@@ -13,18 +20,24 @@ APP_CONFIG_PATH="./build.config"
 echo "CURRENT_VERSION=$CURRENT_VERSION" > "$APP_CONFIG_PATH"
 
 cd Example/NavigationExample
+checkExit
 
 xcrun simctl shutdown all
 sh ../../Scripts/build.sh -c Debug -p NavigationExample -test 'platform=iOS Simulator,name=iPhone 15,OS=17.2'
+checkExit
 
 xcrun simctl shutdown all
 sh ../../Scripts/build.sh -c Debug -p NavigationExample -test 'platform=iOS Simulator,name=iPhone 15,OS=17.0.1'
+checkExit
 
 xcrun simctl shutdown all
 sh ../../Scripts/build.sh -c Debug -p NavigationExample -test 'platform=iOS Simulator,name=iPhone 14,OS=16.4'
+checkExit
 
 xcrun simctl shutdown all
 sh ../../Scripts/build.sh -c Debug -p NavigationExample -test 'platform=iOS Simulator,name=iPhone 14,OS=16.0'
+checkExit
 
 xcrun simctl shutdown all
 sh ../../Scripts/build.sh -c Debug -p NavigationExample -test 'platform=iOS Simulator,name=iPhone 13,OS=15.2'
+checkExit
