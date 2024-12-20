501a858e1fb593f903cd5e7f41e80102207b640d
Sergey Balalaev <sof.bix@mail.ru>
2024-12-20 12:29:40 +0400

    Example update: to circle navigation

--- Example/ListDetais/ListDetails/DetailsView.swift
+++ Example/ListDetais/ListDetails/DetailsView.swift
@@ -6,13 +6,35 @@
 //
 
 import SwiftUI
+import SUINavigation
 
 struct DetailsView: View {
 
     var text: String
 
+    @State
+    var isNext = false
+
     var body: some View {
-        Text(text)
+        VStack {
+            Text(text)
+            Button {
+                isNext = true
+            } label: {
+                Text("to Next")
+            }
+        }
         .navigationTitle("Details")
+        .navigation(isActive: $isNext) {
+            ListView()
+        }
+    }
+}
+
+struct NextView: View {
+
+    var body: some View {
+        Text("Next")
+        .navigationTitle("Next")
     }
 }
